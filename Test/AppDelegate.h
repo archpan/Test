3ed5f5bd72c8d1dac3d778214bc33d768811090c
panguo <panguo4qiubai@gmail.com>
2017-05-22 21:50:53 +0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Archmage on 2017/5/22.
+//  Copyright © 2017年 Archmage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
